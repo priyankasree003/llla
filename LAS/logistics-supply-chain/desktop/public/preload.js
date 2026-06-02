5c440219d580613a54b29ab69feacfedc86429bb
priyankasree003 <priyankasree003@gmail.com>
2026-06-02 12:43:27 +0530

    Delete LAS/logistics-supply-chain directory

--- LAS/logistics-supply-chain/desktop/public/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcMain } = require('electron');
-
-contextBridge.exposeInMainWorld('electron', {
-  store: {
-    get: (key) => ipcMain.invoke('store:get', key),
-    set: (key, value) => ipcMain.invoke('store:set', key, value)
-  }
-});
